437ee46e26aa2d4f83fb72f371bb652b3fbec908
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-01-21 19:04:38 +0530

    Updated env

--- backend/src/main/java/com/swapnil/Classroom/ClassroomApplication.java
+++ backend/src/main/java/com/swapnil/Classroom/ClassroomApplication.java
@@ -1,13 +1,23 @@
 package com.swapnil.Classroom;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
 
 @SpringBootApplication
+@EnableScheduling
 public class ClassroomApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(ClassroomApplication.class, args);
+		System.out.println("FIREBASE_PRIVATE_KEY_PATH: " + System.getenv("FIREBASE_PRIVATE_KEY_PATH"));
+
 	}
 
 }
