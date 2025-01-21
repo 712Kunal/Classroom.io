cd8279bd881756d15ec5a7dd34f6fe7e03f319bc
Vighnesh Brahme <v8ganesh@gmail.com>
2025-01-21 13:31:07 +0530

    add(frontend): replace icons

--- frontend/src/Components/core/Sidebar.jsx
+++ frontend/src/Components/core/Sidebar.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
-import { LuSquareChevronRight, LuSquareChevronLeft, LuCircleUser } from "react-icons/lu";
+import { LuSquareChevronRight, LuSquareChevronLeft } from "react-icons/lu";
+import { MdLibraryBooks } from "react-icons/md";
+import { IoIosNotifications } from "react-icons/io";
+import { PiPathBold } from "react-icons/pi";
+
 const Sidebar = () => {
   const [isOpen, setIsOpen] = React.useState(true);
   const { pathname } = useLocation();
@@ -25,11 +29,13 @@ const Sidebar = () => {
   const routes = [
     {
       name: 'Library',
-      path: '/app/library'
+      path: '/app/library',
+      icon: <MdLibraryBooks/>,
     },
     {
       name: 'Notifications',
-      path: '/app/notifications'
+      path: '/app/notifications',
+      icon: <IoIosNotifications/>,
     }
   ]
 
@@ -39,23 +45,23 @@ const Sidebar = () => {
       <div className="main flex flex-col gap-2 flex-grow">
         <SidebarSection heading={'Library'}>
           {routes.map((route, index) => (
-            <SidebarItem key={index} link={route.path} name={route.name} active={pathname === route.path} />
+            <SidebarItem key={index} open={isOpen} link={route.path} name={route.name} active={pathname === route.path} icon={route.icon} />
           ))}
         </SidebarSection>
         <SidebarSection heading={'Pathways'}>
           {pathways.map((pathway, index) => (
-            <SidebarItem key={index} link={`/app/library/pathways/${pathway.id}`} name={pathway.name} />
+            <SidebarItem key={index} open={isOpen} link={`/app/library/pathways/${pathway.id}`} name={pathway.name} active={pathname === `/app/library/pathways/${pathway.id}`} />
           ))}
         </SidebarSection>
       </div>
-      <SidebarFooter user={user} />
+      <SidebarFooter user={user} open={isOpen} />
     </aside>
   )
 }
 
 export default Sidebar
 
-const SidebarHeader = () => {
+const SidebarHeader = ({ open, setOpen }) => {
   return (
     <div className='header flex justify-between p-2 h-12'>
       {
@@ -63,23 +69,25 @@ const SidebarHeader = () => {
           <img className='object-contain h-full' src="/brand/logo.png" alt="logo" />
         </div>
       }
-      <div className="sidebarToggle grid place-items-center hover:text-violet-500">
+      <div className="sidebarToggle grid place-items-center hover:text-violet-500" onClick={() => setOpen(!open)}>
         {open ? <LuSquareChevronLeft size={24} /> : <LuSquareChevronRight size={24} />}
       </div>
     </div>
   );
 }
 
-const SidebarFooter = ({user}) => {
+const SidebarFooter = ({ user, open }) => {
   return (
-    <div className='footer flex justify-between p-2 gap-2 self-end'>
+    <div className='footer w-full flex justify-between p-2 gap-2 self-end'>
       <div className="icon h-12 w-12">
         <img src={user.avatar} alt="avatar" className='object-contain h-full' />
       </div>
-      <div className="user-details flex flex-col text-sm">
-        <p className='text-lg'>{user.username}</p>
-        <p>{user.email}</p>
-      </div>
+      {
+        open && <div className="user-details flex flex-col text-sm">
+          <p className='text-lg'>{user.username}</p>
+          <p>{user.email}</p>
+        </div>
+      }
     </div>
   );
 }
@@ -95,11 +103,12 @@ const SidebarSection = ({ heading, children }) => {
   );
 }
 
-const SidebarItem = ({ link, name, active }) => {
+const SidebarItem = ({ open, link, name, active, icon }) => {
   return (
     <li className='flex items-center'>
-      <Link to={link} className={`w-full border-2 border-neutral-700 p-2 text-xl rounded-lg ${active ? 'bg-violet-950 text-white' : 'hover:translate-x-2 hover:bg-violet-950'}`}>
-        {name}
+      <Link to={link} className={`flex items-center gap-2 w-full border-2 border-neutral-700 p-2 text-xl rounded-lg ${active ? 'bg-violet-950 text-white' : 'hover:translate-x-2 hover:bg-violet-950'}`}>
+        <div className="icon text-xl">{icon}</div>
+        {open && name}
       </Link>
     </li>
   );
