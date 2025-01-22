e10a02fa41fd5b88db3a12852fc0f7513504285b
Vighnesh Brahme <v8ganesh@gmail.com>
2025-01-22 21:47:40 +0530

    update

--- frontend/src/Pages/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function ProfilePage() {
   return (
-    <div className="text-4xl p-2 w-full h-full border-2 border-neutral-700 rounded-lg">
+    <div className="text-4xl w-full h-full p-2 rounded-lg">
       ProfilePage
     </div>
   );
