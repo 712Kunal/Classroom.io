3e8329724a68b72e70dd94d8d34c72327b5f4e01
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-01-26 00:04:49 +0530

    Notification model and In-app notification

--- backend/src/main/java/com/swapnil/Classroom/service/NotificationService.java
+++ backend/src/main/java/com/swapnil/Classroom/service/NotificationService.java
@@ -1,12 +1,20 @@
 package com.swapnil.Classroom.service;
 
 
+import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QuerySnapshot;
+import com.swapnil.Classroom.controller.PathwayController;
 import com.swapnil.Classroom.entity.Notification;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
@@ -16,6 +24,8 @@ import java.util.concurrent.ExecutionException;
 public class NotificationService {
 
     private final Firestore firestore;
+    private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
+
 
     public void save(Notification notification){
 
@@ -57,4 +67,29 @@ public class NotificationService {
     }
 
 
+    public void sendTaskCompletionNotification(String userId, Long taskId, Notification notification) {
+
+
+        try{
+
+            String notificationId="notif"+taskId+System.currentTimeMillis();
+
+            Date notificationSendDate = Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant());
+
+            notification.setNotificationId(notificationId);
+            notification.setUserId(userId);
+            notification.setNotificationReason(Notification.NotificationReason.TASK);
+            notification.setNotificationSendDate(notificationSendDate);
+            notification.setNotificationReadDate(null);
+            notification.setRelatedEntity(String.valueOf(taskId));
+            notification.setDescription("You have completed a task!");
+
+            save(notification);
+
+            logger.info("Notification send for taskId: "+taskId+" to userId: "+userId);
+        }
+        catch (Exception e) {
+            logger.error("Error sending task completion notification", e);
+        }
+    }
 }
