2a620a9637e5dcc081dda4a2c97e167ed933db9e
Drkadam07 <drkadam04@gmail.com>
2025-01-25 17:30:30 +0530

    Dummy data updated

--- frontend/src/Pages/ProfilePage.jsx
+++ frontend/src/Pages/ProfilePage.jsx
@@ -1,5 +1,5 @@
 // ProfilePage.jsx
-import Navbar from '@/Components/core/Navbar';
+import Navbar from '@/Components/Profile/Navbar';
 import React from 'react';
 
 function ProfilePage() {
@@ -9,7 +9,7 @@ function ProfilePage() {
     email: 'johndoe08@gmail.com',
     FullName: 'John Doe',
     Contact_No: '+1234567890',
-    Bio: 'A passionate developer who loves coding and learning new technologies.',
+    Bio: 'A passionate developer who loves coding and learning new technologies.A bio on social media refers to a short description of a user. It is typically displayed on a users profile page and can include information such as the user s name, location, interests, website links, and contact information.',
     DOB: '1990-01-01', 
     Gender: 'M',
     SocialLinks: {
@@ -27,12 +27,17 @@ function ProfilePage() {
       Occupation: 'Software Engineer',
       LanguagesKnown: ['English', 'Spanish'],
       preferredLearningStyle: ['Interactive Exercises', 'Video Tutorials'],
-      Skills: ['JavaScript', 'React', 'Node.js'],
+      Skills: ['JavaScript', 'React', 'Node.js','Express.js','HTML','Java'],
       Hobbies: ['Coding', 'Reading', 'Gaming'],
       Interests: ['AI', 'Blockchain', 'Web Development']
     },
     pointsAwarded: 1500,
-    badgesAwarded: ['Top Contributor', 'Best Learner'],
+    // badgesAwarded: ['Top Contributor', 'Best Learner','Best Learner','Best Learner'],
+    badgesAwarded: [
+      'https://picsum.photos/200',
+      'https://picsum.photos/200',
+      'https://picsum.photos/200',
+    ],
     badgesAwardedDates: ['2023-01-01', '2023-12-01']
   };
 
