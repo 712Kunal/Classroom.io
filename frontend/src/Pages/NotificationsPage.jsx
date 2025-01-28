b5aa59f985cb1334e4b05181d8306df4790d80bd
712Kunal <jadhavkun0@gmail.com>
2025-01-29 01:03:37 +0530

    added the dialog box for the notification

--- frontend/src/Pages/NotificationsPage.jsx
+++ frontend/src/Pages/NotificationsPage.jsx
@@ -4,6 +4,15 @@ import dummyNotif from '../assets/data/dummyNotif.json';
 import { Bell, Route, LayoutList, Coins, Award } from 'lucide-react';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@/Components/ui/card';
 import { Badge } from '@/Components/ui/badge';
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter
+} from '@/Components/ui/dialog';
 
 function NotificationsPage() {
   const [notifications, setNotifications] = useState(dummyNotif.notifications);
@@ -44,28 +53,44 @@ function NotificationsPage() {
                   animate={{ opacity: 1, y: 0 }}
                   exit={{ opacity: 0, x: -100 }}
                   transition={{ duration: 0.8 }}
+                  key={notification._id}
                   className="animated-div">
-                  <Card className="mb-4 relative overflow-hidden rounded-lg shadow-sm border-slate-600 border-[1px] hover:cursor-pointer hover:border-blue-500 dark:hover:border-blue-300 hover:shadow-blue-200 transition-all duration-500">
-                    <CardContent className="p-4 pr-12">
-                      <div className="flex flex-start gap-4">
-                        {getIcon(notification.notificationReason)}
-                        <div className="flex-1">
-                          <h3 className="font-semibold">{notification.notificationReason}</h3>
-                          <p className="text-sm text-slate-600 dark:text-slate-400">
-                            {notification.description}
-                          </p>
-                          <div className="flex items-center gap-2 mt-2">
-                            <Badge variant="outline" className="p-2 bg-slate-100 dark:bg-slate-900">
-                              Message
-                            </Badge>
-                            <span className="time text-xs text-slate-500">
-                              {notification.notifSendDate}
-                            </span>
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Card className="mb-4 relative overflow-hidden rounded-lg shadow-sm border-slate-600 border-[1px] hover:cursor-pointer hover:border-blue-500 dark:hover:border-blue-300 hover:shadow-blue-200 transition-all duration-500">
+                        <CardContent className="p-4 pr-12">
+                          <div className="flex flex-start gap-4">
+                            {getIcon(notification.notificationReason)}
+                            <div className="flex-1">
+                              <h3 className="font-semibold">{notification.notificationReason}</h3>
+                              <p className="text-sm text-slate-600 dark:text-slate-400">
+                                {notification.description}
+                              </p>
+                              <div className="flex items-center gap-2 mt-2">
+                                <Badge
+                                  variant="outline"
+                                  className="p-2 bg-slate-100 dark:bg-slate-900">
+                                  Message
+                                </Badge>
+                                <span className="time text-xs text-slate-500">
+                                  {notification.notifSendDate}
+                                </span>
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                      </div>
-                    </CardContent>
-                  </Card>
+                        </CardContent>
+                      </Card>
+                    </DialogTrigger>
+                    <DialogContent className="max-w-xs lg:max-w-4xl shadow-2xl shadow-black dark:shadow-blue-400 w-fit">
+                      <DialogHeader className="flex flex-col gap-2">
+                        <DialogTitle className="text-2xl flex items-center gap-2">
+                          {getIcon(notification.notificationReason)}
+                          {notification.notificationReason}
+                        </DialogTitle>
+                        <DialogDescription>{notification.description}</DialogDescription>
+                      </DialogHeader>
+                    </DialogContent>
+                  </Dialog>
                 </motion.div>
               );
             })}
