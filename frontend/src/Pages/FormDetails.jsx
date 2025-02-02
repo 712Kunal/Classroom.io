a58980af15181cf9d0cb9ba23ac40e401018b03f
712Kunal <jadhavkun0@gmail.com>
2025-02-03 00:23:24 +0530

    done with whole user details form

--- frontend/src/Pages/FormDetails.jsx
+++ frontend/src/Pages/FormDetails.jsx
@@ -13,6 +13,7 @@ import {
 import { Input } from '../Components/ui/input2';
 import { Label } from '../Components/ui/label2';
 import BirthDate from '../Components/originUi/birth-date';
+import Languages from '../Components/originUi/languages-known';
 
 function FormDetails() {
   const handleSubmit = (e) => {
@@ -22,7 +23,7 @@ function FormDetails() {
   };
 
   return (
-    <div className="w-full h-full border-2 border-yellow-200 py-4 px-4">
+    <div className="w-full h-full py-4 px-4">
       <div className="header w-full flex flex-col gap-1">
         <h1 className="text-3xl font-bold flex items-center gap-1">
           <ReceiptText className="text-3xl text-black dark:text-blue-500" /> User Details Form
@@ -32,10 +33,11 @@ function FormDetails() {
         </p>
       </div>
 
-      <div className="formContainder w-full p-2 border mt-4">
-        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-          <div className="outer-container w-full lg:w-1/2 flex flex-col gap-2">
-            <div className="Personal-details border-2 rounded-md p-4 flex flex-col gap-4">
+      <div className="formContainder w-full p-2 mt-4">
+        <form className="flex gap-2" onSubmit={handleSubmit}>
+          <div className="outer-container w-full flex flex-wrap">
+            {/* left Section */}
+            <div className="Personal-details border w-full lg:w-1/2 rounded-md p-4 flex flex-col gap-4">
               <h2 className="text-xl dark:text-green-500 flex items-center">
                 <FileUser />
                 Personal Details :
@@ -76,39 +78,77 @@ function FormDetails() {
                 </select>
               </div>
             </div>
-            <div className="social-links border-2 rounded-md p-4 flex flex-col gap-4">
-              <h2 className="text-xl dark:text-pink-500 flex items-center">
-                <MessageCircleHeart />
-                Social Links :
-              </h2>
-              <div className="flex flex-wrap gap-2 justify-center items-center">
-                <div className="flex items-center gap-1">
-                  <Instagram className="text-red-400" />
-                  <Input id="insta" placeholder="https://www.instagram.com/" type="text" />
-                </div>
-                <div className="flex items-center gap-1">
-                  <Github className="text-slate-400" />
-                  <Input id="git" placeholder="https://github.com/" type="text" />
-                </div>
-                <div className="flex items-center gap-1">
-                  <Linkedin className="text-blue-400" />
-                  <Input id="git" placeholder="https://linkedin.com/" type="text" />
-                </div>
-                <div className="flex items-center gap-1">
-                  <Twitter className="text-orange-400" />
-                  <Input id="twitter" placeholder="https://twitter.com/" type="text" />
-                </div>
-                <div className="flex items-center gap-1">
-                  <BriefcaseBusiness className="text-cyan-200" />
-                  <Input id="portfolio" placeholder="https://portfolio.com/" type="text" />
+            {/*  Right Section */}
+            <div className="border w-full lg:w-1/2 rounded-md p-4 flex flex-col gap-4">
+              <div className="social-links border-2 rounded-md p-4 flex flex-col gap-4">
+                <h2 className="text-xl dark:text-pink-500 flex items-center">
+                  <MessageCircleHeart />
+                  Social Links :
+                </h2>
+                <div className="flex flex-wrap gap-2 justify-center items-center">
+                  <div className="flex items-center gap-1">
+                    <Instagram className="text-red-400" />
+                    <Input id="insta" placeholder="https://www.instagram.com/" type="text" />
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <Github className="text-slate-400" />
+                    <Input id="git" placeholder="https://github.com/" type="text" />
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <Linkedin className="text-blue-400" />
+                    <Input id="git" placeholder="https://linkedin.com/" type="text" />
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <Twitter className="text-orange-400" />
+                    <Input id="twitter" placeholder="https://twitter.com/" type="text" />
+                  </div>
+                  <div className="flex items-center gap-1">
+                    <BriefcaseBusiness className="text-cyan-200" />
+                    <Input id="portfolio" placeholder="https://portfolio.com/" type="text" />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="background border-2 rounded-md p-4 flex flex-col gap-4">
-              <h2 className="text-xl dark:text-yellow-400 flex items-center">
-                <GraduationCap /> Background :
-              </h2>
-              
+              <div className="background border-2 rounded-md p-4 flex flex-col gap-4">
+                <h2 className="text-xl dark:text-yellow-400 flex items-center">
+                  <GraduationCap /> Background :
+                </h2>
+                <div>
+                  <Label htmlFor="study">Field of study: </Label>
+                  <Input id="study" placeholder="Eg: Computer Science" type="text" />
+                </div>
+                <div>
+                  <Label htmlFor="degree">Degree: </Label>
+                  <Input id="degree" placeholder="Eg: B.tech" type="text" />
+                </div>
+                <div>
+                  <Label htmlFor="experience">Year's of experience: </Label>
+                  <Input id="experience" placeholder="Eg: 10" type="number" />
+                </div>
+                <div>
+                  <Label htmlFor="occupation">Occupation: </Label>
+                  <Input id="occupation" placeholder="Eg: Software Developer" type="text" />
+                </div>
+                <div>
+                  <Label htmlFor="languages">Language's known: </Label>
+                  <Languages placeholder="Eg: Hindi, English, Marathi" />
+                </div>
+                <div>
+                  <Label htmlFor="learningStyes">Preffered learning Styles: </Label>
+                  <Languages placeholder="Eg: Video Tutorials, Written Guides, Interactive Exercises" />
+                </div>
+                <div>
+                  <Label htmlFor="skills">Technical Skills: </Label>
+                  <Languages placeholder="Eg: React, JavaScript, Node.js" />
+                </div>
+                <div>
+                  <Label htmlFor="hobies">Hobies: </Label>
+                  <Languages placeholder="Eg: Drawing, Gyming, Cooking" />
+                </div>
+                <div>
+                  <Label htmlFor="skills">Interest: </Label>
+                  <Languages placeholder="Eg: Devops, Cloud computing, AI" />
+                </div>
+              </div>
             </div>
           </div>
         </form>
