eeb8317b90e419c8a21fb20b6d0af25833cd5331
Swapnil Dhamal <swapnildhamal58@gmail.com>
2025-02-06 15:46:46 +0530

    Entity updation

--- backend/src/main/java/com/swapnil/Classroom/entity/UserRegistration.java
+++ backend/src/main/java/com/swapnil/Classroom/entity/UserRegistration.java
@@ -1,11 +1,11 @@
-package com.swapnil.Classroom.entity;
-
-import lombok.Data;
-
-@Data
-public class UserRegistration {
-
-    private String username;
-    private String email;
-    private String password;
-}
+//package com.swapnil.Classroom.entity;
+//
+//import lombok.Data;
+//
+//@Data
+//public class UserRegistration {
+//
+//    private String username;
+//    private String email;
+//    private String password;
+//}
